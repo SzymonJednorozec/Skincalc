94fd623c9eaf6dfa25dbb9f066f36fa1fca9b226
Jakub <jakub.konecki9@gmail.com>
2026-02-15 17:50:16 +0100

    feat: add currency rate table| chore: add image to items and update date for prices

--- backend/models.py
+++ backend/models.py
@@ -13,6 +13,7 @@ class Items(Base):
     __tablename__ = "items"
     id = Column(Integer,primary_key=True)
     name = Column(String(100),unique=True)
+    image_url = Column(String(400))
 
 class Markets(Base):
     __tablename__ = "markets"
@@ -26,9 +27,16 @@ class Prices(Base):
     item_id = Column(Integer,ForeignKey('items.id'))
     market_id = Column(Integer,ForeignKey('markets.id'))
     price = Column(Float)
+    update_date = Column(DateTime)
 
     market = relationship("Markets")
 
     @hybrid_property
     def price_after_fee(self):
         return self.price * (1-self.market.fee)
+
+class CurrencyRate(Base):
+    __tablename__ = "currency"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20),unique=True)
+    rate = Column(Float)
