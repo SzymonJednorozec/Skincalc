6790f6fcf8cdc0e16bef549d0120923cb40c0469
Jakub <jakub.konecki9@gmail.com>
2026-02-15 15:32:03 +0100

    feat: add models for final database

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,34 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
 import datetime
 
 class Test(Base):
     __tablename__  = "test"
     id = Column(Integer,primary_key=True)
     name = Column(String)
+
+class Items(Base):
+    __tablename__ = "items"
+    id = Column(Integer,primary_key=True)
+    name = Column(String(100),unique=True)
+
+class Markets(Base):
+    __tablename__ = "markets"
+    id = Column(Integer,primary_key=True)
+    name = Column(String(50))
+    fee = Column(Float)
+
+class Prices(Base):
+    __tablename__ = "prices"
+    id = Column(Integer,primary_key=True)
+    item_id = Column(Integer,ForeignKey('items.id'))
+    market_id = Column(Integer,ForeignKey('markets.id'))
+    price = Column(Float)
+
+    market = relationship("Markets")
+
+    @hybrid_property
+    def price_after_fee(self):
+        return self.price * (1-self.market.fee)
