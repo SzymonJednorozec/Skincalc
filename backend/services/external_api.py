fcad734557b409fd452e41895365758aee913b52
Jakub <jakub.konecki9@gmail.com>
2026-02-16 19:49:44 +0100

    chore: Refactor Skinport API queries to return structured data

--- backend/services/external_api.py
+++ backend/services/external_api.py
@@ -1,10 +1,10 @@
 import httpx
 
-async def get_skinport_sales_history():
+async def get_skinport_sales_history(market_hash_name: str):
     params = {
         "app_id": 730,
         "currency": "PLN",
-        "market_hash_name": "Glove Case,★ Karambit | Slaughter (Minimal Wear)"
+        "market_hash_name": market_hash_name
     }
     
     headers = {
@@ -24,10 +24,27 @@ async def get_skinport_sales_history():
             print(f"Błąd {response.status_code}: {response.text}")
         response.raise_for_status()
         
-        data = response.json()
+        raw_data = response.json()
 
-        for x in data:
-            print(x)
+        items_info = []
+
+        for item in raw_data:
+
+            hash_name = item.get("market_hash_name")
+            name = hash_name if hash_name else "N/A"
+
+            last_24_h = item.get("last_24_hours")
+            price = last_24_h.get("median") if last_24_h else "N/A"
+
+            price = last_24_h.get("median") if last_24_h else "N/A"
+
+            price_str = str(price) if price is not None else "N/A"
+            item_info = {"name": name, "price": price_str, "img_url": None}
+
+            items_info.append(item_info)
+    return items_info
+
+
 
 async def get_exchange_rate():
     url = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/'
