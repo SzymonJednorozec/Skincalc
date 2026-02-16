19ff2875f22f8ae97534feea61742555520b2ecd
Jakub <jakub.konecki9@gmail.com>
2026-02-16 14:41:25 +0100

    feat: add function to get exchange rate | scanner.py

--- backend/services/scanner.py
+++ backend/services/scanner.py
@@ -28,3 +28,20 @@ async def get_skinport_sales_history():
 
         for x in data:
             print(x)
+
+async def get_exchange_rate():
+    url = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/'
+
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url=url,headers=headers)
+        if response.status_code!=200:
+            print(f"Error: {response.status_code}: {response.text}")
+        response.raise_for_status()
+        
+        data = response.json()
+        return data['rates'][0]['mid']
