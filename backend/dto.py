55e135ae7040707e50c319c15cc8eeb4fd0b67aa
Jakub <jakub.konecki9@gmail.com>
2026-02-18 16:27:36 +0100

    faet: Add item_row schema to dto.py

--- backend/dto.py
+++ backend/dto.py
@@ -1,5 +1,20 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 class TestSchema(BaseModel):
     id: int
     name: str
+
+class item_row(BaseModel):
+    name: str
+    image_url: Optional[str] = None
+    steam_price: float
+    steam_updated: datetime
+    skinport_price: float
+    sell_price_after_fee: float
+    skinport_updated: datetime
+    ratio_percentage: float
+
+    class Config:
+        from_attributes = True
