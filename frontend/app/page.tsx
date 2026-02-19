da1f407fce0cfb818c95e0784326c2a80476da91
Jakub <jakub.konecki9@gmail.com>
2026-02-19 23:14:29 +0100

    feat: Add buttons to trigger backend functions and display items in a table

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,90 +1,193 @@
 "use client";
 import { useEffect, useState } from "react";
-import { testfetch,submitfetch, deleteItem,apiTestFetch } from "./services/api";
+import { testfetch,submitfetch, deleteItem,apiTestFetch, scrapeSteam, syncSkinport, updateCurrencyRatio,item_row, getAllItems } from "./services/api";
 import { testData } from "./types/items";
 
 export default function Home() {
-  const [data, setData] = useState<testData[]>([]);
-  const [submitData, setsubmitData] = useState<testData>({
-    id: 0,
-    name: ""
-  });
+  // const [data, setData] = useState<testData[]>([]);
+  // const [submitData, setsubmitData] = useState<testData>({
+  //   id: 0,
+  //   name: ""
+  // });
+
+  const [items, setItems] = useState<item_row[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const x = await testfetch();
-      setData(x);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const x = await testfetch();
+  //     setData(x);
+  //   }
+  //   fetchData();
+  // }, []);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  // const handleSubmit = async (e: React.FormEvent) => {
+  //   e.preventDefault();
     
-    const formData = new FormData(e.currentTarget);
-    const name = formData.get("skinName") as string;
+  //   const formData = new FormData(e.currentTarget);
+  //   const name = formData.get("skinName") as string;
 
-    if (!name){
-      return;
-    }
+  //   if (!name){
+  //     return;
+  //   }
 
+  //   try{
+  //     const newItem = await submitfetch(name);
+  //     setData((prev) =>  [...prev,newItem]);
+
+  //     // setData((data) => (data ? [...data,newItem] : [newItem]));
+  //     // setsubmitData({id: 0, name: ""})
+  //   }
+  //   catch(error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
+
+  // const handleClick = async (id: number) => {
+  //   try{
+  //     await deleteItem(id)
+  //     setData((data) => data.filter((item) => item.id !== id));
+  //   }
+  //   catch(error){
+  //     console.error("Error:", error);
+  //   }
+  // };
+
+  // const APITest = async () => {
+  //   try {
+  //     await apiTestFetch()
+  //   } catch(error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
+
+  const ScrapeSteam = async () => {
+    setIsLoading(true);
     try{
-      const newItem = await submitfetch(name);
-      setData((prev) =>  [...prev,newItem]);
-
-      // setData((data) => (data ? [...data,newItem] : [newItem]));
-      // setsubmitData({id: 0, name: ""})
+      const response = await scrapeSteam();
+      console.log(response);
     }
     catch(error) {
       console.error("Error:", error);
     }
-  };
+    finally{
+      setIsLoading(false);
+    }
+  }
 
-  const handleClick = async (id: number) => {
+  const SyncSkinport = async () => {
+    setIsLoading(true);
     try{
-      await deleteItem(id)
-      setData((data) => data.filter((item) => item.id !== id));
+      const response = await syncSkinport();
+      console.log(response);
     }
-    catch(error){
+    catch(error) {
       console.error("Error:", error);
     }
-  };
+    finally{
+      setIsLoading(false);
+    }
+  }
 
-  const APITest = async () => {
-    try {
-      await apiTestFetch()
-    } catch(error) {
+  const UpdateCurrencyRatio = async () => {
+    setIsLoading(true);
+    try{
+      const response = await updateCurrencyRatio();
+      console.log(response);
+    }
+    catch(error) {
       console.error("Error:", error);
     }
-  };
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+  const LoadItems = async () => {
+    setIsLoading(true);
+    try{
+      const response = await getAllItems();
+      setItems(response);
+    }
+    catch(error) {
+      console.error("Error:", error);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+  // const GetItemsPage = (page_num) =>{
+
+  // }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
-        <div>
-          {data && data.length > 0 ? (
-            data.map((item)=> (
-                <li key={item.id}>
-                  <span>id:{item.id}  name:{item.name}</span>
-                  <button onClick={() => handleClick(item.id)} className="bg-red-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded ml-2">
-                    x
-                  </button>
-                </li>
-            ))
-          ) : (
-            <p>brak danych</p>
-          )}
-        </div>
-        <form onSubmit={handleSubmit}>
-          <input name="skinName"/>
-          <button type="submit">Submit</button>
-        </form>
-
-        <button onClick={() => APITest()} className="bg-red-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded ml-2">
-          api test
-        </button>
+  <main className="p-8 bg-[#032533]">
+    <h1 className="mb-6 text-5xl font-bold text-[#94d2bd]">SkinCalc</h1>
+    <div className="flex flex-row gap-3">
+      <button className="rounded-3xl bg-[#bb3e03] px-4 py-2 text-white hover:bg-[#9b2226] disabled:bg-gray-400 transition-colors"
+        onClick={UpdateCurrencyRatio}
+        disabled={isLoading}
+      >
+        Update USD/PLN
+      </button>
+      <button className="rounded-3xl bg-[#bb3e03] px-4 py-2 text-white hover:bg-[#9b2226] disabled:bg-gray-400 transition-colors"
+        onClick={ScrapeSteam}
+        disabled={isLoading}
+      >
+        Scrape Steam
+      </button>
+      <button className="rounded-3xl bg-[#bb3e03] px-4 py-2 text-white hover:bg-[#9b2226] disabled:bg-gray-400 transition-colors"
+        onClick={SyncSkinport}
+        disabled={isLoading}
+      >
+        Sync Skinport
+      </button>
+      <button className="rounded-3xl bg-[#bb3e03] px-4 py-2 text-white hover:bg-[#9b2226] disabled:bg-gray-400 transition-colors"
+        onClick={LoadItems}
+        disabled={isLoading}
+      >
+        Refresh Table
+      </button>
     </div>
-    
-  );
+    <div className="mt-7 flex overflow-x-auto rounded-lg shadow-2xl">
+      <table className="w-full border-collapse text-left">
+        <thead className="bg-[#531315] text-white">
+          <tr>
+            <th className="p-3">Name</th>
+            <th className="p-3">Steam price</th>
+            <th className="p-3">Skinport price</th>
+            <th className="p-3">Ratio</th>
+            <th className="p-3">Skinport price after fee</th>
+            <th className="p-3">Steam price Update</th>
+            <th className="p-3">Skinport price Update</th>
+          </tr>
+        </thead>
+        <tbody className="bg-[#001219]">
+          {items.map((item)=>(
+            <tr key={item.name} className="transition-colors border-b border-black hover:bg-[#531315] hover:border-5">
+              <td className="items-center p-3 flex">
+                {item.image_url && <img src={item.image_url} alt="" className="w-15 h-15 object-contain mx-3"/>}
+                <span>{item.name}</span>
+              </td>
+              <td className="items-center p-3">{item.steam_price} zł</td>
+              <td className="items-center p-3">{item.skinport_price} zł</td>
+              <td className="items-center p-3">{item.ratio_percentage} %</td>
+              <td className="items-center p-3">{item.sell_price_after_fee} zł</td>
+              <td className="items-center p-3">
+                {new Date(item.steam_updated).toLocaleTimeString()}
+              </td>
+              <td className="items-center p-3">
+                {new Date(item.skinport_updated).toLocaleTimeString()}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </main>
+);
 }
+
+// colors [#001219] [#005f73] [#0a9396] [#94d2bd] [#e9d8a6] [#ee9b00] [#ca6702] [#bb3e03] [#ae2012] [#9b2226]
