04283c4494055aaa8adcf750c8c45ee2e0611995
Jakub <jakub.konecki9@gmail.com>
2026-02-22 00:00:53 +0100

    feat: small data fixes and logic to update single item row

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
-import { testfetch,submitfetch, deleteItem,apiTestFetch, scrapeSteam, syncSkinport, updateCurrencyRatio,item_row, getAllItems } from "./services/api";
+import { testfetch,submitfetch, deleteItem,apiTestFetch, scrapeSteam, syncSkinport, updateCurrencyRatio,item_row, getAllItems, updateItemRow } from "./services/api";
 import { testData } from "./types/items";
+import { setDefaultAutoSelectFamily } from "net";
 
 export default function Home() {
   const [items, setItems] = useState<item_row[]>([]);
@@ -50,10 +51,23 @@ export default function Home() {
   }
 
   const LoadItems = async () => {
-    setIsLoading(true);
     try{
       const response = await getAllItems();
       setItems(response);
+    }
+    catch(error) {
+      console.error("Error:", error);
+    }
+  }
+
+  const UpdateItemRow = async (hash_name: string) =>{
+    setIsLoading(true);
+    try{
+      const response = await updateItemRow(hash_name);
+      setItems((prev) => {
+      const filtered = prev.filter((item) => item.name !== hash_name);
+      return [response,...filtered];
+      });
     }
     catch(error) {
       console.error("Error:", error);
@@ -68,7 +82,7 @@ export default function Home() {
   // }
 
   return (
-  <main className="p-8 bg-[#032533]">
+  <main className="p-8 bg-[#032533] h-screen">
     <h1 className="mb-6 text-5xl font-bold text-[#94d2bd]">SkinCalc</h1>
     <div className="flex flex-row gap-3">
       <button className="rounded-3xl bg-[#bb3e03] px-4 py-2 text-white hover:bg-[#9b2226] disabled:bg-gray-400 transition-colors"
@@ -111,7 +125,7 @@ export default function Home() {
         </thead>
         <tbody className="bg-[#001219]">
           {items.map((item)=>(
-            <tr key={item.name} className="transition-colors border-b border-black hover:bg-[#531315] hover:border-5">
+            <tr key={item.name} onClick={()=>{UpdateItemRow(item.name)}} className="transition-colors border-b border-black hover:bg-[#531315] hover:border-5">
               <td className="items-center p-3 flex">
                 {item.image_url && <img src={item.image_url} alt="" className="w-15 h-15 object-contain mx-3"/>}
                 <span>{item.name}</span>
@@ -121,10 +135,12 @@ export default function Home() {
               <td className="items-center p-3">{item.ratio_percentage} %</td>
               <td className="items-center p-3">{item.sell_price_after_fee} zł</td>
               <td className="items-center p-3">
-                {new Date(item.steam_updated).toLocaleTimeString()}
+                <span className="mx-0.5">{new Date(item.steam_updated).toLocaleTimeString()}</span>
+                <span className="mx-0.5">{new Date(item.steam_updated).toLocaleDateString()}</span>
               </td>
               <td className="items-center p-3">
-                {new Date(item.skinport_updated).toLocaleTimeString()}
+                <span className="mx-0.5">{new Date(item.skinport_updated).toLocaleTimeString()}</span>
+                <span className="mx-0.5">{new Date(item.skinport_updated).toLocaleDateString()}</span>
               </td>
             </tr>
           ))}
