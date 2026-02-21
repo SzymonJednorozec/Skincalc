a1519b4f06ad39e0c99606fa6e28dfb8750d26c9
Jakub <jakub.konecki9@gmail.com>
2026-02-22 00:02:14 +0100

    feat:  add function to extract single itrem_row vairable form db

--- backend/services/crud.py
+++ backend/services/crud.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from typing import List
 from services.utils import clean_price
+from services.enums import Market
 
 def database_upsert(item_list: List, db: Session, market_name: str):
 
@@ -58,3 +59,33 @@ def database_upsert(item_list: List, db: Session, market_name: str):
     
     db.commit()
     print(f"Zakończono upsert dla {len(item_list)} przedmiotów.")
+
+def get_item_row(hash_name, db: Session):
+    item = db.query(Items).filter(Items.name==hash_name).first()
+
+    steam_price_rec = db.query(Prices).join(Markets).filter(
+        Prices.item_id == item.id,
+        Markets.name == "STEAM"
+    ).first()
+
+    skinport_price_rec = db.query(Prices).join(Markets).filter(
+        Prices.item_id == item.id,
+        Markets.name == "SKINPORT"
+    ).first()
+    
+    if steam_price_rec and skinport_price_rec and steam_price_rec.price > 0:
+        
+        skinport_net = skinport_price_rec.price_after_fee
+        ratio = (skinport_net / steam_price_rec.price) * 100
+        result = {
+            "name": item.name,
+            "image_url": item.image_url,
+            "steam_price": round(steam_price_rec.price, 2),
+            "steam_updated": steam_price_rec.update_date,
+            "skinport_price": round(skinport_price_rec.price, 2),
+            "sell_price_after_fee": round(skinport_net, 2),
+            "skinport_updated": skinport_price_rec.update_date,
+            "ratio_percentage": round(ratio, 2)
+        }
+        return result
+
