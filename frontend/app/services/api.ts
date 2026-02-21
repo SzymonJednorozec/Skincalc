6adf918d6202a9d18c38eec91a0e07f01f2d38ff
Jakub <jakub.konecki9@gmail.com>
2026-02-22 00:00:21 +0100

    feat: service function for updating row

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -124,3 +124,16 @@ export async function syncSkinport(): Promise<string>{
     const data = await response.json();
     return data.message;
 }
+
+export async function updateItemRow(hash_name: string): Promise<item_row>{
+    const response = await fetch(`http://127.0.0.1:8000/api/update-item/?hash_name=${hash_name}`,
+        {
+            method: "POST",
+        }
+    );
+    if(!response.ok){
+        throw new Error("Something went wrong | updateItemRow()");
+    }
+    const data = await response.json();
+    return data;
+}
